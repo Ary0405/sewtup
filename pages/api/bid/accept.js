5da77cf7627b2c175181e1d613a5d8a4fefa7998
Sujith <ps335@snu.edu.in>
2023-11-13 22:17:37 +0530

    add accept bid routes

---
+++ pages/api/bid/accept.js
@@ -0,0 +1,29 @@
+import { acceptBid } from "@/services/bid.service";
+import { isOrderByUser } from "@/services/order.service";
+
+
+export default async function bidAccept(req, res) {
+
+    const { orderId, bidId, userId } = req.body;
+
+    if (!orderId || !bidId || !userId) {
+        res.status(400).json({ error: 'Missing body parameter' });
+        return;
+    }
+
+    try {
+        const order = await isOrderByUser(orderId, userId);
+
+        if (!order) {
+            res.status(401).json({ error: 'Not authorized' });
+            return;
+        }
+
+        const bid = await acceptBid(bidId);
+        res.status(200).json({ bid });
+    }
+    catch (error) {
+        res.status(400).json({ error });
+    }
+
+}
