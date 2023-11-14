5f9e869ba895cf8221e34d0aac1f226fb2246d82
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-11-15 03:47:20 +0530

    Add Bid component to Home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Test from "@/components/Test/Test";
+import Bid from "@/components/Bid/Bid";
 
 export default function Home() {
   return (
     <>
-      Meow
-      <Test />
+      {/* Meow
+      <Test /> */}
+      <div style={{width: "80%", backgroundColor: "black"}}><Bid /></div>
     </>
-  )
+  );
 }
