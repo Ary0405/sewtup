41c22d8cb441e287a8e693ba2fd77f53a31da19a
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2023-11-16 01:21:19 +0530

    complete Bid.js and Bid.scss

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Test from "@/components/Test/Test";
-import Bid from "@/components/Bid/Bid";
 
 export default function Home() {
   return (
     <>
-      {/* Meow
-      <Test /> */}
-      <div style={{width: "80%", backgroundColor: "black"}}><Bid /></div>
+      Meow
+      <Test />
     </>
   );
 }
