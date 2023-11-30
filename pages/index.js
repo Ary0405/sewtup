07371e52f1e04da2a5f5a3bd62e4b280a6a79642
Sujith <ps335@snu.edu.in>
2023-12-01 02:52:58 +0530

    meet the team

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import MainHero from "@/components/MainHero/MainHero";
 import { useState } from "react";
 import MainFoot from "@/components/MainFoot/MainFoot";
 import MainCust from "@/components/MainCust/MainCust";
+import MainMeet from "@/components/MainMeet/MainMeet";
 import Image from 'next/image';
 
 export default function Home() {
@@ -78,6 +79,9 @@ export default function Home() {
           </div>
           <div className="section">
             <MainCust />
+          </div>
+          <div className="section">
+            <MainMeet />
           </div>
           <div className="section">
             <img src="/Images/multiple_models.png" alt="main_model2" className="Main__content--img" />
