fb53b70c104e2330fa2a0840ab29bb598eb48f0e
Sujith <ps335@snu.edu.in>
2023-12-01 14:53:19 +0530

    draft mission

--- components/MainMission/MainMission.js
+++ components/MainMission/MainMission.js
@@ -4,15 +4,8 @@ import './MainMission.scss'
 function MainMission() {
     return (
         <div>
-            <div className='MainMission'>
-                <div className='MainMission__sub1'>
-                    <h1>OUR MISSION -</h1>
-                    <p>Empowering Expression Through Customization</p>
-                </div>
-                <div className='MainMission__sub2'>
-                    Our mission is simple but powerful: to empower individuals to express themselves authentically through their clothing. We understand that style is a deeply personal form of self-expression, and everyone deserves to wear something as unique as they are. Sew't Up is the canvas where your style story unfolds.
-                    <hr />
-                </div>
+            {/* <div className='MainMission'> */}
+            {/* 
 
                 <div className='MainMission__sub3'>
                     <h1>What Sets Us Apart - </h1>
@@ -36,7 +29,58 @@ function MainMission() {
                     <img src='/down.svg' alt='down' />
                 </div>
 
+            </div> */}
+            <div className='MainMission'>
+                <div className='MainMission__sub1'>
+                    <div className='MainMission__sub1--sub1'>
+                        <h1>OUR MISSION -</h1>
+                        <p>&nbsp; Empowering </p>
+                        <p>Expression Through Customization</p>
+                    </div>
+                    <div className='MainMission__sub1--sub2'>
+                        <img src='/down.svg' alt='down' />
+                    </div>
+                    <div className='MainMission__sub1--sub3'>
+                        Our mission is simple but powerful: to empower individuals to express themselves authentically through their clothing. We understand that style is a deeply personal form of self-expression, and everyone deserves to wear something as unique as they are. Sew't Up is the canvas where your style story unfolds.
+                        <br />
+                        <br />
+                        <hr />
+                    </div>
+                </div>
+                <div className='MainMission__sub2'>
+                    <div className='MainMission__sub2--sub1' style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        flexWrap: 'wrap',
+                    }}>
+                        <h1>What Sets Us Apart - &nbsp;</h1>
+                        <p>Connecting </p>
+                        <p>Designers with Style Seekers</p>
+                    </div>
+                    <div className='MainMission__sub2--sub2'>
+                        <img src='/top_right.svg' alt='down' />
+                    </div>
+                    <div className='MainMission__sub2--hr'>
+                        <br/>
+                        <hr />
+                    </div>
+                </div>
+                <div className='MainMission__sub3'>
+                    <div className='MainMission__sub3--sub1' style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        flexWrap: 'wrap',
+                    }}>
+                        <h1>Key Features - &nbsp;</h1>
+                        <p>Tailored Experiences </p>
+                        <p>for Designers and Customers</p>
+                    </div>
+                    <div className='MainMission__sub3--sub2'>
+                        <img src='/down.svg' alt='down' />
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
