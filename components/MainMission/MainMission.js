4ddc95fec4350c27617bbfe61944e62c4ba2ebed
Sujith <ps335@snu.edu.in>
2023-12-03 00:41:46 +0530

    images fix

--- components/MainMission/MainMission.js
+++ components/MainMission/MainMission.js
@@ -4,32 +4,6 @@ import './MainMission.scss'
 function MainMission() {
     return (
         <div>
-            {/* <div className='MainMission'> */}
-            {/* 
-
-                <div className='MainMission__sub3'>
-                    <h1>What Sets Us Apart - </h1>
-                    <p>Connecting Designers with Style Seekers</p>
-                    <hr />
-                </div>
-
-                <div className='MainMission__sub4'>
-                    <h1>Key Features — </h1>
-                    <p>Tailored Experiences for Designers and Customers</p>
-                    <hr />
-                </div>
-
-                <div className='MainMission__sub5'>
-                    <img src='/down.svg' alt='down' />
-                </div>
-                <div className='MainMission__sub6'>
-                    <img src='/top_right.svg' alt='down' />
-                </div>
-                <div className='MainMission__sub7'>
-                    <img src='/down.svg' alt='down' />
-                </div>
-
-            </div> */}
             <div className='MainMission'>
                 <div className='MainMission__sub1'>
                     <div className='MainMission__sub1--sub1'>
