79d75e85f3b756055590c2da022340801ff6035b
Sujith <ps335@snu.edu.in>
2024-02-20 18:43:16 +0530

    nav bar links

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Navbar.scss'
+import { useRouter } from 'next/router'
 
 function Navbar() {
+
+  const router = useRouter()
+
   return (
     <div className='Navbar'>
-        <div className='Navbar__logo'>
+        <div className='Navbar__logo large'>
             SEW'TUP.
+        </div>
+        <div className='Navbar__logo mobile'>
+            S.
         </div>
         <div className='Navbar__links'>
             <p className='Navbar__links--link'>Home</p>
@@ -15,8 +22,12 @@ function Navbar() {
             <p className='Navbar__links--link'>Privacy Policy</p>
         </div>
         <div className='Navbar__buttons'>
-            <p className='Navbar__buttons--button'>Login</p>
-            <p className='Navbar__buttons--button'>Register</p>
+            <p className='Navbar__buttons--button'
+                onClick={() => router.push('/auth/login')}
+            >Login</p>
+            <p className='Navbar__buttons--button'
+                onClick={() => router.push('/auth/register')}
+            >Register</p>
         </div>
     </div>
   )
