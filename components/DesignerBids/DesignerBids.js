8199ef46678704da51bd860893633409110656cb
Sujith <ps335@snu.edu.in>
2024-02-23 03:01:09 +0530

    dashbaord  add

---
+++ components/DesignerBids/DesignerBids.js
@@ -0,0 +1,55 @@
+import DashHeader from '../DashTable/DashHeader/DashHeader'
+import DashRow from '../DashTable/DashRow/DashRow'
+import './DesignerBids.scss'
+
+function DesignerBids({ userBids }) {
+    return (
+        <div>
+            <div className='DesignerBids__header'>
+                My Bids
+            </div>
+            <div className="DesignerBids">
+                <DashHeader
+                    style={{
+                        padding: "14px 0 14px 20px",
+                        gap: "20px",
+                        borderTop: "none",
+                    }}
+                    headerTitles={[
+                        "Project Name",
+                        "Amount",
+                        "Days",
+                        "Proposal",
+                        "Status",
+                    ]}
+                />
+                <div className='DesignerBids__body'>
+                    {
+                        userBids.map((bid, index) => (
+                            <DashRow
+                                key={index}
+                                index={bid.id}
+                                className="DesignerBids__body__row"
+                                data={[
+                                    bid.Order.title,
+                                    bid.amount,
+                                    bid.days,
+                                    bid.proposal,
+                                    bid.status,
+                                ]}
+                                style={{
+                                    padding: "14px 0 14px 0",
+                                }}
+                                type={1}
+                            />
+                        ))
+                    }
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default DesignerBids
