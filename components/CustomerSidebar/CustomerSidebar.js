ce3dd0351f4c8c6441b34aaa9906c790df8df89f
its-ChaTTy <ct765@snu.edu.in>
2024-04-08 23:51:08 +0530

    Added Respon@media (max-width: 768px) {

--- components/CustomerSidebar/CustomerSidebar.js
+++ components/CustomerSidebar/CustomerSidebar.js
@@ -22,10 +22,10 @@ export default function CustomerSidebar({ user, option = 0 }) {
                     </div>
                     : null}
                 <div className="CustomerSidebar__top__row">
-                    <Image className="CustomerSidebar__top__row--image" src={'/Images/Customer/bid_management.webp'} width={20} height={20} />
+                    {/* <Image className="CustomerSidebar__top__row--image" src={'/Images/Customer/bid_management.webp'} width={20} height={20} />
                     <Link href="/">
                         <p className="CustomerSidebar__top__row--text">Bid Management</p>
-                    </Link>
+                    </Link> */}
                 </div>
             </div>
             <div className="CustomerSidebar__bottom">
